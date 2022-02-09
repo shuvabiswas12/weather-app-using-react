2102ed4c6547eec1d2637389dc47e4a20a6ccc1b
Shuva_Biswas <shuvabiswas12@gmail.com>
2022-02-09 11:11:21 +0600

    some rerender bug has been fixed.

--- src/components/AllDaysLongWeather.js
+++ src/components/AllDaysLongWeather.js
@@ -1,15 +1,12 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import API_KEY from "../utilities/apiKey";
-import getTemperature from "../utilities/temperatureFormatter";
-import iconUrl from "../utilities/iconUrl";
 import HourlyWeatherBox from "./weatherBox/HourlyWeatherBox";
 import NextDaysWeather from "./NextDaysWeather";
 
 export default function AllDaysLongWeather({locationData}) {
     const [hourlyWeather, setHourlyWeather] = useState({});
     const [dailyWeather, setDailyWeather] = useState({});
-    const [dataIsSet, setDataIsSet] = useState(false);
 
     useEffect(() => {
         if (Object.keys(locationData).length > 0) {
@@ -17,14 +14,13 @@ export default function AllDaysLongWeather({locationData}) {
                 .then((response) => {
                     setHourlyWeather(() => response.data.hourly)
                     setDailyWeather(() => response.data.daily)
-                    setDataIsSet(() => true)
                 })
                 .catch((err) => {
                     alert("All day's long weather file has some problem when fetching data!")
                     console.log(err)
                 })
         }
-    }, [dataIsSet, locationData])
+    }, [locationData])
 
     return (
         <>
