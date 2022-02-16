703784424aa930d7cd282525fc0baedcd17f3dfa
Shuva_Biswas <shuvabiswas12@gmail.com>
2022-02-16 23:09:46 +0600

    update css in copyright section.

--- src/components/copyRight/CopyRight.js
+++ src/components/copyRight/CopyRight.js
@@ -2,17 +2,21 @@ import React from "react";
 
 export default function CopyRight() {
     return (
-        <div className="container">
-            <div className="d-flex justify-content-between flex-wrap">
-                <div className="">
+        <div className="container mt-4">
+            <div className="row justify-content-between">
+                <div className="col-sm-12 col-md-4">
                     <p className="lead">@copyright 2022 </p>
                 </div>
-                <div className="">
-                    <p className="lead text-white-50">Built by <a className="lead text-white" target="_blank" href="https://linkedin.com/in/shuva-biswas">Shuva Biswas</a></p>
-                </div>
-                <div className="">
-                    <a className="text-white" target="_blank" href="https://github.com/shuvabiswas12/weather-app-using-react"><strong>GITHUB link</strong></a>
-                </div>
+               <div className="d-flex justify-content-between">
+                   <div className="col-sm-6 col-md-4">
+                       <p className="lead text-white-50">Built by <a className="lead text-white" target="_blank" href="https://linkedin.com/in/shuva-biswas">Shuva Biswas</a></p>
+                   </div>
+                   <div className="col-sm-6 col-md-4">
+                       <p className="text-end">
+                           <a className="text-white" target="_blank" href="https://github.com/shuvabiswas12/weather-app-using-react"><strong>GITHUB link</strong></a>
+                       </p>
+                   </div>
+               </div>
             </div>
         </div>
     )
